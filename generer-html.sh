fec4735878b5fa58e471364d29bafae3d59d57fb
thjbbvlt <thibault2@ik.me>
2024-08-30 11:43:54 +0200

    ajoute script génération HTML à partir de markdown

---
+++ generer-html.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# génère le html à partir du fichier markdown à l'aide de pandoc
+# https://pandoc.org/
+# (le CSL pour le style de citation provient de l'archive zotero.)
+
+pandoc -i activites.md -o activites.html \
+    --citeproc --bibliography=./bibliography.json --csl=./unige-ll-fr-mod.csl \
+    --standalone --css=./style.css --template=./template.html
