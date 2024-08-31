19167995ec1ffb92d3e09fd7bf82f9ba02be2668
thjbbvlt <thibault2@ik.me>
2024-08-31 15:16:48 +0200

    module dimensions.js

--- js/composition-roles.js
+++ js/composition-roles.js
@@ -1,5 +1,37 @@
+/* je travaille à partir du code trouvé ici:
+ * https://observablehq.com/@d3/force-directed-graph/
+ * */
+
 // const div = d3.select("#composition-roles").node()
 const div = d3.select("#composition-roles")
 
-const path_composition = "../data/composition-roles/mots_composes.csv";
+const path_composition =
+"../data/composition-roles/mots_composes.csv";
 const path_occurrence = "../data/composition-roles/occurrences.csv";
+
+import {
+  color
+} from "./colors.js";
+
+d3.csv(path_composition).then(data => {
+
+  let nodes = [];
+  let id;
+  let lookup = {};
+
+  d3.csv(path_occurrence).then(occurrences => {
+      for (let d of occurrences) {
+          nodes.push(d)
+      }
+  })
+
+  // let edges = [];
+  // let d = {};
+  // /* je vais utiliser des identifiants numériques, que je vais simplement construire par incrémentation. */
+  // let id = 0;
+  //
+  //   for (let d of data) {
+  //       nodes.push(d)
+  //   }
+
+})
--- js/vectors/function.js
+++ js/vectors/function.js
@@ -15,24 +15,17 @@ import {
   color
 } from "../colors.js";
 
+import {
+  body_padding,
+  margin,
+  width,
+  height
+} from "../dimensions.js";
 
-const body_padding = 20;
 const size = {
   default: 2,
   hover: 8
 }
-
-/* propriétés du svg: longueur, largeur. elles sont calculées dynamiquement en prenant en compte la largeur et la hauteur de la fenêtre du navigateur.*/
-const margin = {
-    top: 10,
-    right: 10,
-    bottom: 30,
-    left: 30
-  },
-  width = Math.min(800, window.innerWidth) - margin.left - margin
-  .right - (body_padding * 2),
-  height = Math.min(width, window.innerHeight - margin.top - margin
-    .bottom)
 
 function make_scatterplot_embeddings(datapath, corpora, mots, id) {
 
