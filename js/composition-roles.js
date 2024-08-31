8fbb2362ec8d29f2fd3fa01f364252ecd84e9dd2
thjbbvlt <thibault2@ik.me>
2024-08-31 17:43:09 +0200

    recupéré un script pour directed graph + enleevé parties

--- js/composition-roles.js
+++ js/composition-roles.js
@@ -1,44 +1,100 @@
-/* je travaille à partir du code trouvé ici:
- * https://observablehq.com/@d3/force-directed-graph/
- * */
-
 import {
   color
 } from "./colors.js";
-
 import {
   body_padding,
   margin,
   width,
   height
-} from "../dimensions.js";
+} from "./dimensions.js";
+
+/* je travaille à partir du code trouvé ici:
+ * https://observablehq.com/@d3/force-directed-graph/
+* */
 
 // const div = d3.select("#composition-roles").node()
 const div = d3.select("#composition-roles")
 
-const path_composition = "../data/composition-roles/mots_composes.csv";
-const path_occurrence = "../data/composition-roles/occurrences.csv";
+const path_composition = "./data/composition-roles/mots_composes.csv";
+const path_occurrence = "./data/composition-roles/occurrences.csv";
 
+const data = {links: [{source: "a", target: "b"}, {source: "b", target: "c"}], nodes: [{id: "a"}, {id: "b"}, {id: "c"}, {id: "d"}]}
+const links = data.links.map(d => ({...d}));
+const nodes = data.nodes.map(d => ({...d}));
 
-d3.csv(path_composition).then(data => {
+  // Create a simulation with several forces.
+  const simulation = d3.forceSimulation(nodes)
+      .force("link", d3.forceLink(links).id(d => d.id))
+      .force("charge", d3.forceManyBody())
+      .force("center", d3.forceCenter(width / 2, height / 2))
+      .on("tick", ticked);
 
-  let nodes = [];
-  let id;
-  let lookup = {};
+  // Create the SVG container.
+  const svg = d3.create("svg")
+      .attr("width", width)
+      .attr("height", height)
+      .attr("viewBox", [0, 0, width, height])
+      .attr("style", "max-width: 100%; height: auto;");
 
-  d3.csv(path_occurrence).then(occurrences => {
-    for (let d of occurrences) {
-      nodes.push(d)
-    }
-  })
+  // Add a line for each link, and a circle for each node.
+  const link = svg.append("g")
+      .attr("stroke", "#999")
+      .attr("stroke-opacity", 0.6)
+    .selectAll()
+    .data(links)
+    .join("line")
+      .attr("stroke-width", 1);
 
-  // let edges = [];
-  // let d = {};
-  // /* je vais utiliser des identifiants numériques, que je vais simplement construire par incrémentation. */
-  // let id = 0;
-  //
-  //   for (let d of data) {
-  //       nodes.push(d)
-  //   }
+  const node = svg.append("g")
+      .attr("stroke", "#fff")
+      .attr("stroke-width", 1.5)
+    .selectAll()
+    .data(nodes)
+    .join("circle")
+      .attr("r", 5)
+      .attr("fill", "red");
 
-})
+  node.append("title")
+      .text(d => d.id);
+
+  // Add a drag behavior.
+  node.call(d3.drag()
+        .on("start", dragstarted)
+        .on("drag", dragged)
+        .on("end", dragended));
+
+  // Set the position attributes of links and nodes each time the simulation ticks.
+  function ticked() {
+    link
+        .attr("x1", d => d.source.x)
+        .attr("y1", d => d.source.y)
+        .attr("x2", d => d.target.x)
+        .attr("y2", d => d.target.y);
+
+    node
+        .attr("cx", d => d.x)
+        .attr("cy", d => d.y);
+  }
+
+  // Reheat the simulation when drag starts, and fix the subject position.
+  function dragstarted(event) {
+    if (!event.active) simulation.alphaTarget(0.3).restart();
+    event.subject.fx = event.subject.x;
+    event.subject.fy = event.subject.y;
+  }
+
+  // Update the subject (dragged node) position during drag.
+  function dragged(event) {
+    event.subject.fx = event.x;
+    event.subject.fy = event.y;
+  }
+
+  // Restore the target alpha so the simulation cools after dragging ends.
+  // Unfix the subject position now that it’s no longer being dragged.
+  function dragended(event) {
+    if (!event.active) simulation.alphaTarget(0);
+    event.subject.fx = null;
+    event.subject.fy = null;
+  }
+
+div.node().append(svg.node());
