a5da03e361ce4ddf58636096c7c61b7676875636
thjbbvlt <thibault2@ik.me>
2024-08-31 13:39:26 +0200

    déplacé scripts/modules vecteurs

--- js/colors.js
+++ js/colors.js
@@ -2,4 +2,5 @@ export const color = {
   forum: "#69b3a2",
   general: "#ff00ff",
   axistitle: "#ffffff",
+  fg: '#ffffff',
 };
---
+++ js/vectors/lire_ecrire.js
@@ -0,0 +1,7 @@
+import {
+  make_scatterplot_embeddings
+} from './function.js';
+
+make_scatterplot_embeddings("./data/vectors/verbes_similarite.json", [
+  "forum", "general"
+], ["lire", "ecrire"], "#vecteurs")
