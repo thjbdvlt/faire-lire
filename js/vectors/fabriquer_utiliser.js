a0d5c77609c2794e262eab8fcded375596d5b0c8
thjbbvlt <thibault2@ik.me>
2024-09-01 15:55:07 +0200

    add new JSON for vectors

---
+++ js/vectors/fabriquer_utiliser.js
@@ -0,0 +1,7 @@
+import {
+  make_scatterplot_embeddings
+} from './scatterplot.js';
+
+make_scatterplot_embeddings("./data/vectors/verbes_similarite_fabriquer_utiliser.json", [
+  "forum", "générique"
+], ["fabriquer", "utiliser"], "#vecteurs-fabriquer-utiliser")
---
+++ js/vectors/produire_consommer.js
@@ -0,0 +1,7 @@
+import {
+  make_scatterplot_embeddings
+} from './scatterplot.js';
+
+make_scatterplot_embeddings("./data/vectors/verbes_similarite_produire_consommer.json", [
+  "forum", "générique"
+], ["produire", "consommer"], "#vecteurs-produire-consommer")
